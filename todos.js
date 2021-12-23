70ca18b590ef64a56e60df6eea6ad5812e48ada0
msolorio <msolorio.github@gmail.com>
2021-12-23 10:01:41 -0800

    Adds boilerplate for azure node circle ci continous deployment

---
+++ todos.js
@@ -0,0 +1,23 @@
+//Comment for Base project
+module.exports = [
+  {
+    id: 1,
+    task: "Fix Sink"
+  },
+  {
+    id: 2,
+    task: "Buy Groceries"
+  },
+  {
+    id: 3,
+    task: "Wash the dishes"
+  },
+  {
+    id: 4,
+    task: "Make Dinner"
+  },
+  {
+    id: 5,
+    task: "Take out the trash"
+  }
+];
